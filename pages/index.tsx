b9886ad0230c84703491914ca3a9dc2fe9ffe620
UniqueDevStorm <storm@stormdev.club>
2021-04-18 21:04:41 +0900

    feat: License

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import Header from "../components/Header";
-import Line from "../components/Line";
 
 export default function Home() {
     return (
         <div>
             <Header />
-            <Line />
         </div>
     )
 }
