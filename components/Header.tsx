b1e47e7d2b8d3a0560a2f246df44208be4b443c2
UniqueDevStorm <storm@stormdev.club>
2021-04-18 21:47:43 +0900

    feat: slogan

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,25 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function Header() {
     return (
         <div>
             <nav className='fixed w-full no-underline'>
                 <div className='flex items-center flex-grow float-left px-0 sm:px-20 sm:p-2'>
                     <img src='/UniqueCode.png' height={80} width={80} className='opacity-100'  alt='UniqueCode'/>
-                    <h1 className='font-black sm:text-2xl'>UniqueCode</h1>
+                    <h1 className='font-black sm:text-3xl'>UniqueCode</h1>
                 </div>
-                <div className='flex float-right font-black p-7 px-5 gap-4 sm:text-xl sm:px-20 sm:p-8'>
+                <div className='flex float-right font-black p-7 px-5 gap-4 sm:text-3xl sm:px-20 sm:p-8'>
                     <a href='./'>프로젝트</a>
                     <a href='./'>멤버</a>
                     <a href='./'>채용</a>
                 </div>
             </nav>
+            <div className='flex mx-auto p-60 pl-96 gap-10 ml-40'>
+                <h1 className='font-black text-6xl leading-normal'>당신의 디스코드에<br />유니크한 재미를 더.</h1>
+                <div className='mt-10'>
+                    <FontAwesomeIcon  icon={["fab", "discord"]} className='text-9xl' color='#738ADB' />
+                </div>
+            </div>
         </div>
     )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import '../styles/globals.css';
 import '../styles/global.css';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { library } from "@fortawesome/fontawesome-svg-core";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+library.add(fab);
+
+export default MyApp;
