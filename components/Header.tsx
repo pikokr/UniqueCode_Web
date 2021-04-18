710bfed866ada446b60ffa5532057eeed4aa23f0
UniqueDevStorm <storm@stormdev.club>
2021-04-18 21:54:48 +0900

    feat: Responsive Web Structure

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,9 +14,9 @@ export default function Header() {
                     <a href='./'>채용</a>
                 </div>
             </nav>
-            <div className='flex mx-auto p-60 pl-96 gap-10 ml-40'>
-                <h1 className='font-black text-6xl leading-normal'>당신의 디스코드에<br />유니크한 재미를 더.</h1>
-                <div className='mt-10'>
+            <div className='flex mx-auto p-32 sm:p-60 sm:pl-96 gap-10 sm:ml-40'>
+                <h1 className='font-black sm:text-6xl sm:leading-normal'>당신의 디스코드에<br />유니크한 재미를 더.</h1>
+                <div className='sm:mt-10'>
                     <FontAwesomeIcon  icon={["fab", "discord"]} className='text-9xl' color='#738ADB' />
                 </div>
             </div>
