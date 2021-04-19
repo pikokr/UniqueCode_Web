3429c7207fa7ad912511a44bb73fbca6af632739
UniqueDevStorm <storm@stormdev.club>
2021-04-19 09:28:57 +0900

    feat: Project page

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,17 +9,11 @@ export default function Header() {
                     <h1 className='font-black sm:text-3xl'>UniqueCode</h1>
                 </div>
                 <div className='flex float-right font-black p-7 px-5 gap-4 sm:text-3xl sm:px-20 sm:p-8'>
-                    <a href='./'>프로젝트</a>
+                    <a href='/project'>프로젝트</a>
                     <a href='./'>멤버</a>
                     <a href='./'>채용</a>
                 </div>
             </nav>
-            <div className='flex mx-auto p-32 sm:p-60 sm:pl-96 gap-10 sm:ml-40'>
-                <h1 className='font-black sm:text-6xl sm:leading-normal'>당신의 디스코드에<br />유니크한 재미를 더.</h1>
-                <div className='sm:mt-10'>
-                    <FontAwesomeIcon  icon={["fab", "discord"]} className='text-9xl' color='#738ADB' />
-                </div>
-            </div>
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
 import Header from "../components/Header";
+import Bots from "../components/Bots";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export default function Home() {
     return (
-        <div>
-            <Header />
+        <div className='no-underline text-black'>
+            <div className='bg-gray-50'>
+                <Header />
+                <div className='flex mx-auto p-32 sm:p-60 sm:pl-96 gap-10 sm:ml-40'>
+                    <h1 className='font-black text-xs leading-3 sm:text-6xl sm:leading-normal'>당신의 디스코드에<br />유니크한 재미를 더.</h1>
+                    <div className='sm:mt-10'>
+                        <FontAwesomeIcon  icon={["fab", "discord"]} className='text-9xl' color='#738ADB' />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
